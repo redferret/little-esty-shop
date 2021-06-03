904bb682b827aa7b2095cbe70da916929953f8c8
kevn7n <kevn_nguyen@yahoo.com>
2021-06-03 11:39:30 -0600

    Create admin merchants and invoices controller, index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  namespace :merchants do
+  resources :merchants, only: [] do
     resources :dashboard, only: [:index]
     resources :invoices, only: [:index, :show, :update]
     resources :items, only: [:index, :show, :new, :create, :update]
   end
-  
+
   namespace :admin do
     resources :dashboard, only: [:index]
     resources :merchants, only: [:index, :show]
-  end
     resources :invoices, only: [:index, :show, :update]
+  end
 end
