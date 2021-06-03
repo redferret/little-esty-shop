39087bbef9d34015c3cc5bf051e7a46ee9005663
Richard <rdesilvey@gmail.com>
2021-06-03 11:43:01 -0600

    Rearrange specs

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "dashboard" do
   before(:each) do
     @merchant_1 = FactoryBot.create(:merchant)
 
-    visit dashboard_merchant_path(@merchant_1)
+    visit merchant_dashboard_index_path(@merchant_1)
   end
 
   it 'has merchant name' do
@@ -12,14 +12,12 @@ RSpec.describe "dashboard" do
   end
 
   it 'has link to merchant items index' do
-
     expect(page).to have_link('Merchant Items Index')
     click_on('Merchant Items Index')
     expect(current_path).to eq(merchant_items_path(@merchant_1))
   end
 
-    it 'has link to merchant invoices index' do
-
+  it 'has link to merchant invoices index' do
     expect(page).to have_link('Merchant Invoices Index')
     click_on('Merchant Invoices Index')
     expect(current_path).to eq(merchant_invoices_path(@merchant_1))
---
+++ spec/features/merchants/invoices/show_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'show' do
+  before(:each) do
+    @merchant = FactoryBot.create(:merchant)
+
+    @item_1 = FactoryBot.create(:item, merchant: @merchant)
+    @item_2 = FactoryBot.create(:item, merchant: @merchant)
+    @item_3 = FactoryBot.create(:item, merchant: @merchant)
+
+    @customer = FactoryBot.create(:customer)
+
+    @invoice_1 = Invoice.create!(status: "in_progress", customer: @customer)
+    @invoice_item_1 = FactoryBot.create(:invoice_item, item: @item_1, invoice: @invoice_1)
+
+    FactoryBot.create(:invoice_item, item: @item_1, invoice: @invoice_1)
+    FactoryBot.create(:invoice_item, item: @item_2, invoice: @invoice_1)
+    FactoryBot.create(:invoice_item, item: @item_3, invoice: @invoice_1)
+  end
+  
+  before :each do
+    visit merchant_invoice_path(@merchant, @invoice_1)
+  end
+
+  it 'shows the invoice attributes' do
+    within '#invoice-info' do
+      expect(page).to have_content("#{@invoice_1.id}")
+      expect(page).to have_content("#{@invoice_1.status}")
+      expect(page).to have_content("#{@invoice_1.created_at}")
+      expect(page).to have_content("#{@customer.first_name}")
+      expect(page).to have_content("#{@customer.last_name}")
+    end
+  end
+
+  describe 'invoice status form,' do
+    before :each do
+      visit merchant_invoice_path(@merchant, @invoice_1)
+    end
+
+    it 'can update a invoice status' do
+      expect(page).to have_select("invoice[status]", selected: @invoice_1.status)
+      click_on("Update Item Status")
+      expect(current_path).to eq(merchant_invoice_path(@merchant, @invoice_1))
+    end
+  end
+end
