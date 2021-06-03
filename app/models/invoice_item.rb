cf0f0d60dbe95ae9f3785d91f7ea8c1d3fd58441
Richard <rdesilvey@gmail.com>
2021-06-03 13:44:44 -0600

    Remove repeated logic

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,9 +4,4 @@ class InvoiceItem < ApplicationRecord
 
   enum status: { pending: 'pending', packaged: 'packaged', shipped: 'shipped' }
 
-  def self.invoice_items_ready_to_ship(merchant)
-   merchant.invoice_items.where.not(status: :shipped).joins(:invoice)
-   .where('invoices.status = ?', Invoice.statuses[:completed])
-   .order('invoices.created_at')
-  end
 end
