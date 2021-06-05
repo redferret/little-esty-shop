0b830adad8f13c3eb608ebaeab852498ff79b97d
Richard <rdesilvey@gmail.com>
2021-06-05 14:07:16 -0600

    Implement feature(s)

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,23 @@
 class Admin::MerchantsController < ApplicationController
-  def index
+  before_action :set_merchants, only: %i[ index ]
 
+  def index
+  end
+
+  def update
+    if params[:status].present?
+      merchant = Merchant.find(params[:id])
+      merchant.status = params[:status]
+      merchant.save
+      redirect_to admin_merchants_path
+    end
+  end
+
+  private
+
+  def set_merchants
+    @enabled_merchants = Merchant.enabled_merchants
+    @disabled_merchants = Merchant.disabled_merchants
+    @top_merchants = Merchant.top_merchants
   end
 end
---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,10 @@
+module AdminHelper
+  def enable_disable_merchant_link(type, merchant)
+    case type
+    when 'enable'
+      link_to 'Enable', admin_merchant_path(merchant, status: true), method: :patch, class:'btn btn-primary btn-sm'
+    when 'disable'
+      link_to 'Disable', admin_merchant_path(merchant, status: false), method: :patch, class:'btn btn-secondary btn-sm'
+    end
+  end
+end
