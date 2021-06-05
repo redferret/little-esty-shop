94172148bc5a1ee64044404767eb447278da527b
Richard <rdesilvey@gmail.com>
2021-06-05 14:04:02 -0600

    Write feature and model tests

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'The merchants index page,' do
+  before :all do
+    @merchant_1 = FactoryBot.create(:merchant)
+    @merchant_2 = FactoryBot.create(:merchant)
+    @merchant_3 = FactoryBot.create(:merchant, enabled: false, name: 'Disabled 1')
+    @merchant_4 = FactoryBot.create(:merchant, enabled: false, name: 'Disabled 2')
+  end
+
+  before :each do
+    visit admin_merchants_path
+  end
+
+  describe 'enabled merchants list,' do
+    it 'shows all the merchants that are enabled' do
+      within '#enabled-merchants' do
+        expect(page).to have_content('Enabled Merchants')
+
+        expect(page).to_not have_link(@merchant_3.name)
+        expect(page).to_not have_link(@merchant_4.name)
+        
+        within "#merchant-#{@merchant_1.id}" do
+          expect(page).to have_link(@merchant_1.name)
+          expect(page).to have_link('Disable')
+        end
+        
+        within "#merchant-#{@merchant_2.id}" do
+          expect(page).to have_link(@merchant_2.name)
+          expect(page).to have_link('Disable')
+        end
+      end
+    end
+  end
+  
+  describe 'disabled merchants list,' do
+    it 'shows all the merchants that are disabled' do
+      within '#disabled-merchants' do
+        expect(page).to have_content('Disabled Merchants')
+    
+        expect(page).to_not have_link(@merchant_1.name)
+        expect(page).to_not have_link(@merchant_2.name)
+        
+        within "#merchant-#{@merchant_3.id}" do
+          expect(page).to have_link(@merchant_3.name)
+          expect(page).to have_link('Disable')
+        end
+        
+        within "#merchant-#{@merchant_4.id}" do
+          expect(page).to have_link(@merchant_4.name)
+          expect(page).to have_link('Disable')
+        end
+      end
+    end
+  end
+end
---
+++ spec/helpers/admin_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe AdminHelper do
+  before :each do
+    @merchant = FactoryBot.create(:merchant)
+  end
+  describe '::enable_disable_merchant_link' do
+    it 'returns a link to enable a merchant' do
+      expect(enable_disable_merchant_link('enable', @merchant)).to have_link('Enable', href: admin_merchant_path(@merchant, status: true))
+    end
+    it 'returns a link to disable a merchant' do
+      expect(enable_disable_merchant_link('disable', @merchant)).to have_link('Disable', href: admin_merchant_path(@merchant, status: false))
+    end
+  end
+end
