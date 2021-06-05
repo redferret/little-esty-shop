7aa33882835c54f4f9c4db911319f1256f654b28
JoannaCoPo <jococopo@gmail.com>
2021-06-05 15:37:00 -0600

    Spec tests for admin merchant show and edit

---
+++ spec/features/admin/merchants/edit_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Merchants Edit Page' do
+  before :each do
+    @merchant_1 = FactoryBot.create(:merchant)
+  end
+
+  describe 'as an admin' do
+    it "loads prepopulated form with merchant's info" do
+      visit edit_admin_merchant_path(@merchant_1)
+
+      within('#form') do
+        fill_in('name', with: 'Fanzy Petz')
+        click_on 'Submit'
+      end
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+    end
+
+    it 'shows flash message if fields are saved blank' do
+      visit edit_admin_merchant_path(@merchant_1)
+
+      within('#form') do
+        fill_in('name', with: 'Fanzy Petz')
+        click_on 'Submit'
+      end
+
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+      save_and_open_page
+      expect(page).to have_content('Merchant Successfully Updated')
+    end
+  end
+end
+
+
+  # And I see a form filled in with the existing merchant attribute information
+  # When I update the information in the form and I click ‘submit’
+  # Then I am redirected back to the merchant's admin show page where I see the updated information
+  # And I see a flash message stating that the information has been successfully updated.
---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "As an admin" do
+  describe 'when I visit the admin merchants show page' do
+    before :each do
+      @merchant_1 = FactoryBot.create(:merchant)
+
+      visit admin_merchant_path(@merchant_1)
+    end
+
+    it 'displays a link to update the merchant info' do
+
+      expect(page).to have_link('Update Merchant')
+
+      click_link('Update Merchant')
+save_and_open_page
+      expect(current_path).to eq(edit_admin_merchant_path(@merchant_1))
+    end
+  end
+end
+# As an admin,
+# When I visit a merchant's admin show page
+# Then I see a link to update the merchant's information.
+# When I click the link
+# Then I am taken to a page to edit this merchant
+# And I see a form filled in with the existing merchant attribute information
+# When I update the information in the form and I click ‘submit’
+# Then I am redirected back to the merchant's admin show page where I see the updated information
+# And I see a flash message stating that the information has been successfully updated.
