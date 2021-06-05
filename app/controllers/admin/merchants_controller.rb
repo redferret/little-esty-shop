5db5c12c6337938b79aaeff160c01eaed15596fd
JoannaCoPo <jococopo@gmail.com>
2021-06-05 15:39:31 -0600

    Add update action with flash message

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,27 @@ class Admin::MerchantsController < ApplicationController
   def index
 
   end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    if @merchant.update(merchant_params)
+      redirect_to admin_merchant_path(@merchant)
+      flash[:notice] = "Merchant Successfully Updated"
+
+    end
+  end
+
+  private
+
+  def merchant_params
+     params.require(:merchant).permit(:name)
+  end
 end
