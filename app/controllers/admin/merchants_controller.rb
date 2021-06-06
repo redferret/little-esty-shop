ef5e2f5e6b13c1fb5d8c77494c4ce8477cf063df
kevn7n <kevn_nguyen@yahoo.com>
2021-06-06 15:56:41 -0600

    Update admin/merhcants per story_15

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,40 @@
 class Admin::MerchantsController < ApplicationController
+  before_action :set_merchants, only: %i[ index ]
+
   def index
-    @merchants = Merchant.all   
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+
+    if params[:enabled].present?
+      merchant = Merchant.find(params[:id])
+      merchant.enabled = params[:enabled]
+      merchant.save
+      redirect_to admin_merchants_path
+    elsif @merchant.update(merchant_params)
+        flash[:success] = "Merchant Successfully Updated"
+        redirect_to admin_merchant_path(@merchant)
+    end
+  end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  private
+
+  def set_merchants
+    @enabled_merchants = Merchant.enabled_merchants
+    @disabled_merchants = Merchant.disabled_merchants
+    @top_merchants = Merchant.top_merchants
+  end
+
+  def merchant_params
+     params.require(:merchant).permit(:name)
   end
 end
