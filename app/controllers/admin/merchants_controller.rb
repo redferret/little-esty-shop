df1f0ffca04c5d2d0cdae9f3a5b526110761a656
JoannaCoPo <jococopo@gmail.com>
2021-06-07 21:52:57 -0600

    Add #new action

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -17,15 +17,27 @@ class Admin::MerchantsController < ApplicationController
         redirect_to admin_merchant_path(@merchant)
     end
   end
-  
+
   def show
     @merchant = Merchant.find(params[:id])
   end
-  
+
   def edit
     @merchant = Merchant.find(params[:id])
   end
-  
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  # def create
+  #   merchant = Merchant.new(merchant_params)
+  #   if merchant.save
+  #     flash[:notice] = 'New merchant has been created!'
+  #     redirect_to admin_merchants_path
+  #   end
+  # end
+
   private
 
   def set_merchants
@@ -33,7 +45,7 @@ class Admin::MerchantsController < ApplicationController
     @disabled_merchants = Merchant.disabled_merchants
     @top_merchants = Merchant.top_merchants
   end
-  
+
   def merchant_params
      params.require(:merchant).permit(:name)
   end
