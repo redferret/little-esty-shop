8cbec04b40c24348d11216b1c237f1a9d104554a
JoannaCoPo <jococopo@gmail.com>
2021-06-08 16:59:47 -0600

    Psuedocode top_five_customers method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
+
+  def self.top_five_customers
+    #joins with transactions ?
+    # where result: :success
+    # select all customers  - count transaction.result - == top_five
+    #order them -desc? and set the limit(5)
+  end
 end
