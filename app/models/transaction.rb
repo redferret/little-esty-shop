18538f7c04c6f60ec1331590041b0be9a64ec614
JoannaCoPo <jococopo@gmail.com>
2021-06-08 16:59:12 -0600

    Define success_count method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,11 @@ class Transaction < ApplicationRecord
   validates :credit_card_number, presence: true, numericality: true
   validates :result, presence: true
   # validates :credit_card_expiration_date  ##BLANK
-
   enum result: {failed: 'failed', success: 'success'}
 
   belongs_to :invoice
+
+  def self.success_count
+    where(result: :success).count
+  end
 end
