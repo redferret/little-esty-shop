b57b08f28a4bedbf03b382bbb7192d22dfdab6e7
Richard <rdesilvey@gmail.com>
2021-06-09 16:42:10 -0600

    Configure based on the enviornment

--- config/initializers/api_clients.rb
+++ config/initializers/api_clients.rb
@@ -1,7 +1,11 @@
-# Bootup the client for the Github API
-# github_api_client = GithubAPI::V3::Client.new
+# To keep us from over shooting our limited number of API calls
+case ENV.fetch("RAILS_ENV")
+when 'development'
+  PROJECT_REPO = {'name'=>'little-esty-shop', 'html_url'=>'https://github.com/redferret/little-esty-shop'}
+when 'production'
+  # Bootup the client for the Github API
+  github_api_client = GithubAPI::V3::Client.new
 
-# Get the project repo
-PROJECT_REPO = {'name'=>'little-esty-shop', 'html_url'=>'https://github.com/redferret/little-esty-shop', 'commits_url'=>''}
-# PROJECT_REPO = github_api_client.project_repo
+  PROJECT_REPO = github_api_client.project_repo
+end
 
