0ea73b4f2c3bd331b7859c42f1d46cefbb1eaf72
Richard <rdesilvey@gmail.com>
2021-06-09 16:03:16 -0600

    Add initializer for apis

---
+++ config/initializers/api_clients.rb
@@ -0,0 +1,7 @@
+# Bootup the client for the Github API
+# github_api_client = GithubAPI::V3::Client.new
+
+# Get the project repo
+PROJECT_REPO = {'name'=>'little-esty-shop', 'html_url'=>'https://github.com/redferret/little-esty-shop', 'commits_url'=>''}
+# PROJECT_REPO = github_api_client.project_repo
+
