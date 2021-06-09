b7384d8882f6cccdc423c46f0dccff9ec8413098
Richard <rdesilvey@gmail.com>
2021-06-08 19:56:24 -0600

    Clean up

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -2,9 +2,9 @@ module ItemHelper
   def enable_disable_link(enable, merchant, item)
     case enable
     when true
-      link_to 'Enable', merchant_item_path(merchant, item, status: true), method: :patch, class:'btn btn-primary btn-sm'
+      link_to 'Enable', merchant_item_path(merchant, item, status: enable), method: :patch, class:'btn btn-primary btn-sm'
     when false
-      link_to 'Disable', merchant_item_path(merchant, item, status: false), method: :patch, class:'btn btn-secondary btn-sm'
+      link_to 'Disable', merchant_item_path(merchant, item, status: enable), method: :patch, class:'btn btn-secondary btn-sm'
     end
   end
 end
